4e24198c5b7cb8aaa027271abb44bb03481217ad
NGGYOK <nggyok@gmail.com>
2024-01-19 19:37:13 +0800

    添加日志器

---
+++ Logger.cpp
@@ -0,0 +1,39 @@
+#include "Logger.h"
+#include <chrono>
+#include <ctime>
+void Logger::debug(const std::string&& message)
+{
+	std::clog << GetCurrentTime() << "\033[2m[DEBUG] " << message << "\033[m";
+}
+
+void Logger::info(const std::string&& message)
+{
+	std::clog << GetCurrentTime() << "[INFO] " << message;
+}
+
+void Logger::warning(const std::string&& message)
+{
+	std::clog << GetCurrentTime() << "\033[33m[WARNING] " << message << "\033[m";
+}
+
+void Logger::error(const std::string&& message)
+{
+	std::cerr << GetCurrentTime() << "\033[31;5m[ERROR]\033[m \033[31m" << message << "\033[m";
+}
+
+void Logger::fatal(const std::string&& message)
+{
+	std::cerr << GetCurrentTime() << "\033[101;5m[FATAL]\033[m \033[91;1m" << message << "\033[m";
+}
+
+const std::string& Logger::GetCurrentTime()
+{
+	auto currentTime = std::chrono::system_clock::now();
+	auto time_t = std::chrono::system_clock::to_time_t(currentTime);
+	tm tm{}; localtime_s(&tm,&time_t);
+
+	static std::string formatTime{};
+	formatTime = std::format("[{}年{}月{}日{:0>2}时{:0>2}分{:0>2}秒]",
+		tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_yday + 1, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	return formatTime;
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <Windows.h>
+#include "Logger.h"
 #include "KeyManager.h"
 
 int main(int argc, char* argv[]) {
-	std::cout << "按任意键初始化键盘设备\n";
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		Logger::debug(std::format("{}\n", i));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		Logger::info(std::format("{}\n", i));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		Logger::warning(std::format("{}\n", i));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		Logger::error(std::format("{}\n", i));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		Logger::fatal(std::format("{}\n", i));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+
+	Logger::info("按任意键初始化键盘设备\n");
 	KeyManager::InitKeyborad();
-	std::cout << "初始化键盘设备完成\n";
+	Logger::info("初始化键盘设备完成\n");
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 	KeyManager::PushKey(VK_CONTROL);
 	KeyManager::PushKey(VK_MENU);
@@ -15,10 +31,9 @@ int main(int argc, char* argv[]) {
 	KeyManager::PopKey(VK_CONTROL);
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 	KeyManager::SendKey(VK_ESCAPE);
-
-	std::cout << "移动或按任意键初始化鼠标设备\n";
+	Logger::info("移动或按任意键初始化鼠标设备\n");
 	KeyManager::InitMouse();
-	std::cout << "初始化鼠标设备完成\n";
+	Logger::info("初始化鼠标设备完成\n");
 	
 	KeyManager::MoveAbsolute(500,500);
 
