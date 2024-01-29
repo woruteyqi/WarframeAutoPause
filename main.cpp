d27d41b30e74deb6d6f3abd70c9a9dcc463d7886
NGGYOK <nggyok@gmail.com>
2024-01-29 13:29:40 +0800

    修复release模式下开核桃无法停止

--- main.cpp
+++ main.cpp
@@ -19,14 +19,14 @@ int main(int argc, char* argv[]) {
 		"注意：游戏应为无边框全屏模式\n"
 		"注意：检测到红氧会发出声音，连续检测红氧3次触发暂停\n\n"
 		"快捷键：\n"
-		"U->查询地形（查询到有声音提示）\n"
+		"U->查询地形（查询到和未查询到有不一样的声音提示）\n"
 		"I->自动开核桃（鼠标点击-回车）循环，使用随机延迟\n"
 		"O->锁定鼠标位置（锁定绝对坐标为第一个核桃位置，禁止相对移动，同时会保持游戏窗口激活并置于顶层）\n"
 		"P->退出程序\n\n");
 	while (1)
 	{
 		UINT key{ KeyManager::LastKeyUp() };
-		static bool flag[2]{false};
+		static bool flagOpenRelic{ false }, flagLockMouse{false};
 		static int exitcount{ 0 };
 
 		switch (key)
@@ -35,23 +35,25 @@ int main(int argc, char* argv[]) {
 			//Logger::debug("抬起U\n");
 			std::thread([] {
 				Logger::info("开始寻找虚空地形\n");
-				static EEparser ep; auto test = ep.CheckVoidTerrain();
-				if (test.empty())
+				static EEparser ep; 
+				const auto&& Terrains = ep.CheckVoidTerrain();
+				const auto&& GetFreq = [](int HalfSteps)
+				{
+					const int A4{ 440 };
+					int Freq{ static_cast<int>(std::pow((float)2, (float)HalfSteps / (float)12) * (float)A4) };
+					Logger::debug(std::format("HalfSteps: {} Freq：{}\n", HalfSteps, Freq));
+					return Freq;
+				};
+				if (Terrains.empty())
 				{
 					Logger::info("未找到地形\n");
+					Beep(GetFreq(5), 250);
+					Beep(GetFreq(-2), 250);
 				}
 				int count{ 0 };
-				for (const auto& it : test)
+				for (const auto& it : Terrains)
 				{
-					
 					Logger::warning(std::format("找到{}\n", it));
-					const auto&& GetFreq = [](int HalfSteps)
-					{
-						const int A4{ 440 };
-						int Freq{ static_cast<int>(std::pow((float)2, (float)HalfSteps / (float)12) * (float)A4) };
-						Logger::debug(std::format("HalfSteps: {} Freq：{}\n", HalfSteps, Freq));
-						return Freq;
-					};
 					Beep(GetFreq(3 + count), 500);
 					Beep(GetFreq(7 + count), 500);
 					Beep(GetFreq(10 + count), 500);
@@ -61,20 +63,23 @@ int main(int argc, char* argv[]) {
 			break;
 		case 'I':
 			//Logger::debug("抬起I\n");
-			flag[0] = !flag[0];
-			flag[0] ? Logger::info("开始开核桃\n") : Logger::info("停止开核桃\n");
-
-			static const auto&& OpenRelic = [&] {
-				std::thread([&] {
+			flagOpenRelic = !flagOpenRelic;
+			flagOpenRelic ? Logger::info("开始开核桃\n") : Logger::info("停止开核桃\n");
+			static const auto&& OpenRelic = [] {
+				std::thread([] {
 					std::random_device rd;
 					while (1)
 					{
-						if (flag[0])
+						if (flagOpenRelic)
 						{
 							KeyManager::SendKey(VK_LBUTTON, std::uniform_int_distribution<>(20, 50)(rd)); Logger::debug("输入VK_LBUTTON\n");
 							std::this_thread::sleep_for(std::chrono::milliseconds(std::uniform_int_distribution<>(300, 500)(rd)));
 							KeyManager::SendKey(VK_RETURN, std::uniform_int_distribution<>(20, 50)(rd)); Logger::debug("输入VK_RETURN\n");
 							std::this_thread::sleep_for(std::chrono::milliseconds(std::uniform_int_distribution<>(1500, 3000)(rd)));
+						}
+						else
+						{
+							std::this_thread::sleep_for(std::chrono::seconds(1));
 						}
 					}
 				}).detach();
@@ -83,15 +88,15 @@ int main(int argc, char* argv[]) {
 			break;
 		case 'O':
 			//Logger::debug("抬起O\n");
-			flag[1] = !flag[1];
-			flag[1] ? Logger::info("锁定鼠标位置\n") : Logger::info("解锁鼠标位置\n");
-			static const auto&& LockMouse = [&]{
-				std::thread([&] {
+			flagLockMouse = !flagLockMouse;
+			flagLockMouse ? Logger::info("锁定鼠标位置\n") : Logger::info("解锁鼠标位置\n");
+			static const auto&& LockMouse = []{
+				std::thread([] {
 					bool locked{false};
 					auto&& pos = ImageProc::nor2scr(0.203125f, 0.25925925f);
 					while (1)
 					{
-						if (flag[1])
+						if (flagLockMouse)
 						{
 							if (!locked)
 							{
@@ -100,13 +105,14 @@ int main(int argc, char* argv[]) {
 							}
 							KeyManager::MoveAbsolute(pos.first, pos.second);//防止回中必须保持移动
 							ImageProc::ActiveWindow();
-							std::this_thread::sleep_for(std::chrono::seconds(1));
 						}
-						else if(locked)
+						else if (locked)
 						{
 							KeyManager::LockMousePosition(false);
 							locked = false;
 						}
+						std::random_device rd;
+						std::this_thread::sleep_for(std::chrono::milliseconds(std::uniform_int_distribution<>(500, 1500)(rd)));
 					}
 				}).detach();
 				return 0;
